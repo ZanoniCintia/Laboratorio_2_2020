f474e33e46384d3027dd2486accdd1ac7b4f061f
zanonicintia <cintu94@hotmail.com>
2020-03-26 01:44:44 -0300

    ejercicios 1 y 2 completos

---
+++ Ejercicios_guia/Ejercicio_1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 1";
+            int numero;
+            int minimo = int.MaxValue;
+            int maximo = int.MinValue;
+            int suma=0;
+            float promedio;
+            int i = 0;
+            string auxiliar;
+
+            do
+            {   
+                Console.WriteLine("Ingrese un numero");
+                auxiliar= Console.ReadLine();
+                if(int.TryParse(auxiliar,out numero))
+                {
+                    if (numero > maximo)
+                    {
+                        maximo = numero;
+                    }
+                    else if (numero < minimo)
+                    {
+                        minimo = numero;
+                    }
+                }
+
+                suma += numero;
+                i++;
+
+            } while(i < 5);
+
+            promedio = suma / 5;
+            Console.WriteLine($"El maximo es :{maximo} El minimo es :{minimo} La suma total es :{suma} El promedio es {promedio}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicios_guia/Ejercicio_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int numero;
+
+            Console.WriteLine("Ingrese un numero");
+            numero = Convert.ToInt32(Console.ReadLine());
+            while (numero < 0)
+            {
+                Console.WriteLine("ERROR. ¡Reingresar número!");
+                numero = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Cuadrado: {0}", Math.Pow(numero, 2));
+            Console.WriteLine("Cubo: {0}", Math.Pow(numero, 3));
+
+            Console.ReadKey();
+        }
+    }
+}
