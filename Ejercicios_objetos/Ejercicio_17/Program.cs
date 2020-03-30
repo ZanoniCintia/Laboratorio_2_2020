e4aaf370f72ae5b4666199038fd418b30266420b
zanonicintia <cintu94@hotmail.com>
2020-03-30 01:32:58 -0300

    Boligrafo sin terminar

--- Ejercicios_objetos/Ejercicio_17/Program.cs
+++ Ejercicios_objetos/Ejercicio_17/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Entidades;
 namespace Ejercicio_17
 {
     class Program
@@ -11,6 +11,15 @@ namespace Ejercicio_17
         static void Main(string[] args)
         {
             Console.Title = "Ejercicio 17";
+            
+            Boligrafo azul = new Boligrafo(ConsoleColor.Blue,100);
+            Boligrafo rojo = new Boligrafo(ConsoleColor.Red, 50);
+            string estrella = "";
+            Console.WriteLine($"Ingrese gasto de tinta: ");
+            string cadena = ( Console.ReadLine());
+            Console.WriteLine(azul.Pintar(short.Parse(cadena), out estrella ));
+            Console.WriteLine(rojo.Pintar(short.Parse(cadena), out estrella));
+            Console.ReadKey();
         }
     }
 }
--- Ejercicios_objetos/Entidades/Boligrafo.cs
+++ Ejercicios_objetos/Entidades/Boligrafo.cs
@@ -8,6 +8,65 @@ namespace Entidades
 {
     public class Boligrafo
     {
+        const short cantidadTintaMaxima = 100;
+        private ConsoleColor color;
+        private short tinta;
+
+        public Boligrafo(ConsoleColor color,short tinta)
+        {
+            color = this.color;
+            tinta = this.tinta;
+        }
+        public ConsoleColor GetColor
+        {
+            get { return this.color; }
+            
+        }
+        public short GetTinta
+        {
+            get { return this.tinta; }
+
+        }
+       
+        private void SetTinta(short tinta)
+        {
+            if (tinta >= 0 && tinta == cantidadTintaMaxima)
+            {
+                if (tinta > 0 && tinta < 100)
+                {
+                    Console.WriteLine("Cargar tinta");
+                }
+                else if (tinta < 0)
+                {
+                    Console.WriteLine("Gastar tinta");
+                }
+            }
+        }
+
+        public void Recargar()
+        {
+            SetTinta(cantidadTintaMaxima);
+        }
+
+        public bool Pintar(short gasto, out string dibujo)
+        {
+           
+            dibujo = "*";
+            if (gasto > 0)
+            {
+                SetTinta(gasto);
+                Console.WriteLine("Puede pintar");
+                for (int i = 0; i < gasto; i++)
+                {
+                    
+                    Console.WriteLine(dibujo);
+                }
+                
+            } 
+            return true;
+        }
+
+
 
     }
 }
