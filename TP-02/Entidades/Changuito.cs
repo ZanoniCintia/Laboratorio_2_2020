bce92ed433e6d8f389edf163941f095cf162accb
zanonicintia <cintu94@hotmail.com>
2020-04-28 17:28:14 -0300

    tp2

---
+++ TP-02/Entidades/Changuito.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2018
+{
+    /// <summary>
+    /// No podrá tener clases heredadas.
+    /// </summary>
+    public class Changuito
+    {
+        List<Producto> productos;
+        int espacioDisponible;
+        public enum ETipo
+        {
+            Dulce, Leche, Snacks, Todos
+        }
+
+        #region "Constructores"
+        private Changuito()
+        {
+            this.productos = new List<Producto>();
+        }
+        public Changuito(int espacioDisponible)
+        {
+            this.espacioDisponible = espacioDisponible;
+        }
+        #endregion
+
+        #region "Sobrecargas"
+        /// <summary>
+        /// Muestro el Changuito y TODOS los Productos
+        /// </summary>
+        /// <returns></returns>
+        public string ToString()
+        {
+            return Changuito.Mostrar(this, ETipo.Todos);
+        }
+        #endregion
+
+        #region "Métodos"
+
+        /// <summary>
+        /// Expone los datos del elemento y su lista (incluidas sus herencias)
+        /// SOLO del tipo requerido
+        /// </summary>
+        /// <param name="c">Elemento a exponer</param>
+        /// <param name="ETipo">Tipos de ítems de la lista a mostrar</param>
+        /// <returns></returns>
+        public string Mostrar(Changuito c, ETipo tipo)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("Tenemos {0} lugares ocupados de un total de {1} disponibles", c.productos.Count, c.espacioDisponible);
+            sb.AppendLine("");
+            foreach (Producto v in c.productos)
+            {
+                switch (tipo)
+                {
+                    case ETipo.Snacks:
+                        sb.AppendLine(v.Mostrar());
+                        break;
+                    case ETipo.Dulce:
+                        sb.AppendLine(v.Mostrar());
+                        break;
+                    case ETipo.Leche:
+                        sb.AppendLine(v.Mostrar());
+                        break;
+                    default:
+                        sb.AppendLine(v.Mostrar());
+                        break;
+                }
+            }
+
+            return sb;
+        }
+        #endregion
+
+        #region "Operadores"
+        /// <summary>
+        /// Agregará un elemento a la lista
+        /// </summary>
+        /// <param name="c">Objeto donde se agregará el elemento</param>
+        /// <param name="p">Objeto a agregar</param>
+        /// <returns></returns>
+        public static Changuito operator +(Changuito c, Producto p)
+        {
+            foreach (Producto v in c)
+            {
+                if (v == p)
+                    return c;
+            }
+
+            c.productos.Add(p);
+            return c;
+        }
+        /// <summary>
+        /// Quitará un elemento de la lista
+        /// </summary>
+        /// <param name="c">Objeto donde se quitará el elemento</param>
+        /// <param name="p">Objeto a quitar</param>
+        /// <returns></returns>
+        public static Changuito operator -(Changuito c, Producto p)
+        {
+            foreach (Producto v in c)
+            {
+                if (v == p)
+                {
+                    break;
+                }
+            }
+
+            return c;
+        }
+        #endregion
+    }
+}
---
+++ TP-02/Entidades/Snacks.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2018
+{
+    public class Snacks
+    {
+        public Snacks(EMarca marca, string patente, ConsoleColor color)
+            : base(patente, marca, color)
+        {
+        }
+        /// <summary>
+        /// Los snacks tienen 104 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 104;
+            }
+        }
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SNACKS");
+            sb.AppendLine(base);
+            sb.AppendLine("CALORIAS : {0}", this.CantidadCalorias);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb;
+        }
+    }
+}
